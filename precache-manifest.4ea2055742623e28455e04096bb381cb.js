c45460a5c833466c1b3845672a020747dc8bfd8b
josemorista <josemorista@id.uff.br>
2020-05-13 17:33:00 -0300

    Updates

---
+++ precache-manifest.4ea2055742623e28455e04096bb381cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4546cb2bc8847828000602ca2677d85",
+    "url": "/moradia-uff-dist-alimentos/index.html"
+  },
+  {
+    "revision": "05eb22dfe1298198846c",
+    "url": "/moradia-uff-dist-alimentos/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b83181836627c9284bd1",
+    "url": "/moradia-uff-dist-alimentos/static/js/2.e577fe4b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/moradia-uff-dist-alimentos/static/js/2.e577fe4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05eb22dfe1298198846c",
+    "url": "/moradia-uff-dist-alimentos/static/js/main.d4990e2e.chunk.js"
+  },
+  {
+    "revision": "bdf8a3dfe166d1093507",
+    "url": "/moradia-uff-dist-alimentos/static/js/runtime-main.4295c8c2.js"
+  }
+]);
