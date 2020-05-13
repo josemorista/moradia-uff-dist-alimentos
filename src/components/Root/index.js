45049f5a977333501dc13ebfac0ded040ec9a531
josemorista <josemorista@id.uff.br>
2020-05-13 19:32:14 -0300

    style fixes

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -31,6 +31,7 @@ export default function Root() {
       }
     }
     setResults(newResults)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [items, total, decimals])
 
   return (
@@ -51,7 +52,7 @@ export default function Root() {
           value={type}
           onChange={(e) => {
             setType(e.target.value)
-            if (e.target.value === 'items') {
+            if (e.target.value === 'itens') {
               setTotal(0)
             }
             setItems([])
@@ -60,7 +61,7 @@ export default function Root() {
           }}
         >
           <MenuItem value="kg">Divisão por Kg</MenuItem>
-          <MenuItem value="items">Divisão por items</MenuItem>
+          <MenuItem value="itens">Divisão por itens</MenuItem>
         </TextField>
         <TextField
           variant="outlined"
@@ -97,7 +98,7 @@ export default function Root() {
             </List>
           </div>
         ) : null}
-        <Typography>Certo, agora adicione os items que deseja dividir e suas respectivas quantidades:</Typography>
+        <Typography>Certo, agora adicione os itens que deseja dividir e suas respectivas quantidades:</Typography>
         <br />
         <Paper variant="outlined" style={{ padding: '20px', paddingLeft: '0px' }}>
           <TextField variant="outlined" label="Nome do item" type="text" style={{ margin: '8px' }} value={item.name || ''} onChange={(e) => setItem({ ...item, name: e.target.value })} />
@@ -116,31 +117,31 @@ export default function Root() {
           </Grid>
         </Paper>
         <br />
-        {type === 'items' ? (
+        {type === 'itens' ? (
           <div>
-            <Typography>Pra fazer as contas preciso saber tambem quantas pessoas moram atualmente na moradia(total). Quantas sao?</Typography>
+            <Typography>Para fazer as contas, preciso saber tambem quantas pessoas moram atualmente na moradia(total). Quantas são?</Typography>
             <br />
             <TextField variant="outlined" label="Quantidade de moradores" type="Number" value={total} onChange={(e) => setTotal(Number(e.target.value))} />
             <br />
             <br />
-            <Typography>Agora, irei perguntar quantas pessoas moram em cada ala e te direi quantos {type} de cada devem ser enviados :)</Typography>
+            <Typography>Agora, irei perguntar quantas pessoas moram em cada ala e informarei quantos {type} de cada devem ser enviados :)</Typography>
           </div>
         ) : (
           <Typography>
-            Agora, irei perguntar quantos {type} irão para cada ala e te direi quantos {type} de cada devem ser enviados :)
+            Agora, irei perguntar quantos {type} irão para cada ala e informarei quantos {type} de cada item devem ser enviados :)
           </Typography>
         )}
 
         <br />
         {results.map((el, index) => (
           <Paper key={index} variant="outlined" style={{ padding: '20px', paddingLeft: '0px', marginBottom: '10px' }}>
-            <Typography style={{ margin: '8px' }} color="primary">
+            <Typography style={{ margin: '8px' }}>
               <b>Ala {index + 1}:</b>
             </Typography>
 
             <TextField
               variant="outlined"
-              label={type === 'items' ? 'Quantidade de pessoas' : 'Quantidade em kg'}
+              label={type === 'itens' ? 'Quantidade de pessoas' : 'Quantidade em kg'}
               type="Number"
               style={{ margin: '8px' }}
               value={results[index].qnt || ''}
